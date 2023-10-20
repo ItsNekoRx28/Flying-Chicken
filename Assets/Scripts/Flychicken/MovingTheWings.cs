2b0e8bd7ac721e5069335ffd9ed38e5a48b702f6
Xacal23 <84201134+Xacal23@users.noreply.github.com>
2023-10-20 14:43:06 +0200

    version 2
    - pollo sube y baja pero no consigo que se desplace hacia delante
    - Seguido por una camara

--- Assets/Scripts/Flychicken/MovingTheWings.cs
+++ Assets/Scripts/Flychicken/MovingTheWings.cs
@@ -1,30 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class MovingTheWings : MonoBehaviour
 {
-    private int n = 0;
-    Vector3 direction;
-    private Rigidbody2D _rigidbody;
-    public float up = 5f;
+    public float speed = 5f; // Velocidad de movimiento hacia adelante
+    public float jumpForce = 10f; // Fuerza de salto
+    private Rigidbody rb; // Referencia al componente Rigidbody
+    private bool isLaunched = false; // Bandera para verificar si el personaje está en el suelo
+    private Vector3 _position;
+    private float move;
 
-
-    // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+
+
     void Update()
     {
+        
 
-        if (Input.GetKeyDown(KeyCode.Space) && n++!=0)
+        // Lanzar el proyectil cuando se presiona la tecla de espacio por primera vez
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-           _rigidbody = GetComponent<Rigidbody2D>();
-            _rigidbody.velocity = transform.up * up * Time.deltaTime;
+            isLaunched = true;
+        }
+
+        if (isLaunched) {
+            // Mover el personaje hacia adelante
+            //rb.AddForce(transform.forward * speed);
+            Vector3 forwardMovement = transform.forward * speed * Time.deltaTime;
+            rb.MovePosition(rb.position + forwardMovement);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isLaunched){
+
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+             
         }
     }
+
 }
