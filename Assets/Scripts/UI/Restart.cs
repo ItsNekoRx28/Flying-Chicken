0be42319c1a8007c64a5b93dd900d058fe0374fd
Nihel KB <nihelkella@gmail.com>
2024-01-06 19:52:24 +0100

    Añadida seccion de codigo a terminar para el rebote por anticaida

--- Assets/Scripts/UI/Restart.cs
+++ Assets/Scripts/UI/Restart.cs
@@ -9,6 +9,19 @@ public class Restart : MonoBehaviour
 {
     public GameUtils gameUtils;
 
+    public float jumpForce = 10f;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        // Asegúrate de que el objeto tiene un componente Rigidbody2D adjunto
+        rb = GetComponent<Rigidbody2D>();
+        if (rb == null)
+        {
+            Debug.LogError("El objeto necesita tener un componente Rigidbody2D adjunto.");
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("floor"))
@@ -16,6 +29,11 @@ public class Restart : MonoBehaviour
             if (!isFallProtectionOn())
             {
                 gameUtils.GameOver();
+            }
+            else
+            {
+                // Aplicar fuerza hacia la derecha para simular el rebote
+                
             }
         }
     }
