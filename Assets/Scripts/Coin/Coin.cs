f91e1373fe182ca7249f94da080632bd9b336e8b
Nihel KB <nihelkella@gmail.com>
2024-01-08 01:32:54 +0100

    Devuelto valor por defecto al conseguir una moneda

--- Assets/Scripts/Coin/Coin.cs
+++ Assets/Scripts/Coin/Coin.cs
@@ -10,7 +10,7 @@ public class Coin : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            GameManager.instance.AddCoin(100);
+            GameManager.instance.AddCoin(1);
             gameObject.SetActive(false);
             Physics2D.IgnoreCollision(other, GetComponent<Collider2D>(), true);
         }
